78faca46a71c79545a7ceb46a8614678fd28c8c3
AbdallahYousef <abdallah7@outlook.com>
2021-01-04 15:05:58 +0200

    3rd Commit

--- js/app.js
+++ js/app.js
@@ -44,6 +44,7 @@ var lima = {
 
 var locations = [seattle, tokyo, dubai, paris, lima];
 var locationsStr = ['Seattle', 'Tokyo', 'Dubai', 'Paris', 'Lima'];
+var hours = ['6Am','7Am','8Am','9Am','10Am','11Am','12Am','1Pm','2Pm','3Pm','4Pm','5Pm','7Pm'];
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -57,11 +58,42 @@ function cookieAmountInt(avg , no){
 }
 
 
+
 for (var index = 0; index < locations.length; index++) {
     console.log((locationsStr[index]));
     for (var i = 6; i < 19; i++) {
-        console.log(i + ' oclock');
-        var cookiesNo = (getRandomInt((locations[index]).minCust,(locations[index]).maxCust) * (locations[index]).avgCookieSale);
+        var clock = (hours[(i-6)]);
+        console.log(clock);
+        var cookiesNo = Math.floor((getRandomInt((locations[index]).minCust,(locations[index]).maxCust) * (locations[index]).avgCookieSale));
         console.log(cookiesNo);
     }
 }
+// trying to midify html via JS. #ME
+
+// selecting the element by it id from our html document
+// var parentElement = document.getElementById('jsEntry');
+
+// // creating a new element
+// // lets create a new article element
+
+// var article = document.createElement('article');
+// parentElement.appendChild(article);
+
+// var h2 = document.createElement('h2');
+// h2.textContent = locationsStr[];
+// article.appendChild(h2);
+
+// var par = document.createElement('p');
+// par.textContent = 'The age of '+ frankie.name + ' is '+ frankie.age;
+// article.appendChild(par);
+
+
+// var list = document.createElement('ul');
+// article.appendChild(list);
+
+
+// for (var index = 0; index < frankie.favFood.length; index++) {
+//     var listItem = document.createElement('li');   
+//     listItem.textContent = frankie.favFood[index];
+//     list.appendChild(listItem);
+// }
